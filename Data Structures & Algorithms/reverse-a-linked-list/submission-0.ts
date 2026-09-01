9d838fd969185673a6a9b31d8e87bc2b6fd8f378
Sepehr Soltanieh <sepsol.works@gmail.com>
2026-09-01 11:16:38 -0700

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head: ListNode | null): ListNode {
+        let prev = null;
+        let curr = head;
+
+        while (true) {
+            if (curr === null) {
+                return prev;
+            }
+
+            const tmp = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = tmp;
+        }
+    }
+}
